bde4c13a89d4cda1e5d22f52c67a7afb6b678c74
Juan Camilo Rico <juancarico92@gmail.com>
2020-10-07 19:00:05 -0500

    feat: mongodb installed and its logic implemented in server.js, pending creation of a mongodb instance and its integration to the project

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 const express = require('express')
+const MongoClient = require('mongodb')
 require('dotenv').config()
 
 const PORT = process.env.PORT
+const DBURL = process.env.DB_URL
+// Se declara la db
+let db
 
 // Se crea la instancia app (puede tener otro nombre)
 const app = express()
+
+// Lógica para conectar la db
+MongoClient.connect(DBURL, {
+  useUnifiedTopology: true
+}, (err, database) => {
+  if (err) return console.error(err)
+  console.log('Connected to Database')
+  db = database
+})
 
 app.get('/', (request, response) => {
   response.send('Hola Mundo')
